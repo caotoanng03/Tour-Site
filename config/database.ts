4e845297a38af00c465e627131f0da91cffa9964
CaoToan <caotoan940@gmail.com>
2023-12-16 10:19:14 +0100

    Database Connection

---
+++ config/database.ts
@@ -0,0 +1,16 @@
+import { Sequelize, DataTypes } from "sequelize";
+
+const sequelize = new Sequelize("tour_site", "root", "", {
+    host: "localhost",
+    port: 3306,
+    dialect: "mysql"
+});
+
+export const connect = async (): Promise<void> => {
+    try {
+        await sequelize.authenticate();
+        console.log("Connection has been established successfully.")
+    } catch (error) {
+        console.error("Unable to connect to database: ", error);
+    }
+}
--- index.ts
+++ index.ts
@@ -1,6 +1,10 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import * as database from "./config/database";
+
 dotenv.config();
+
+database.connect();
 
 const app: Express = express();
 const port: string | number = process.env.PORT || 9100;
