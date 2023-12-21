6095f4779a278ebb2d441493874f433a654b1716
CaoToan <caotoan940@gmail.com>
2023-12-21 18:21:30 +0700

    List tours based on category slug

--- controllers/client/tour.controller.ts
+++ controllers/client/tour.controller.ts
@@ -1,13 +1,43 @@
 import { Request, Response } from "express";
 import Tour from "../../models/tour.model";
+import sequelize from "../../config/database";
+import { QueryTypes } from "sequelize";
 
-// [GET] /tours
-export const index = async (req: Request, res: Response) => {
-    // select * from tours where deleted = false and status = 'active'
-    const tours = await Tour.findAll({ raw: true });
+// [GET] /tours/:slugCategory
+export const getToursBaseOnCategrorySlug = async (req: Request, res: Response): Promise<void> => {
+    const slugCategory: string = req.params.slugCategory;
 
-    res.render("client/pages/tours/index", {
-        pageTitle: "Tour List",
-        tours: tours
+    const tours = await sequelize.query(
+        `
+        SELECT tours.*, categories.title AS categoryTitle, ROUND(price * (1 - discount/100), 0) AS price_special
+        FROM tours
+        JOIN tours_categories ON tours.id = tours_categories.tour_id
+        JOIN categories ON tours_categories.category_id = categories.id
+        WHERE
+            categories.slug = '${slugCategory}'
+            AND categories.deleted = false
+            AND categories.status = 'active'
+            AND tours.deleted = false
+            AND tours.status = 'active';
+        `, {
+        type: QueryTypes.SELECT
     });
+
+    if (tours.length > 0) {
+        tours.forEach(item => {
+            if (item["images"]) {
+                const images = JSON.parse(item["images"]);
+                item["image"] = images[0];
+
+                item["price_special"] = parseFloat(item["price_special"]);
+            };
+        });
+
+        res.render(`client/pages/tours/index`, {
+            pageTitle: "All tours: " + tours[0]["categoryTitle"],
+            tours
+        });
+    } else {
+        res.redirect(`/categories`)
+    }
 };
--- routes/client/tour.route.ts
+++ routes/client/tour.route.ts
@@ -3,6 +3,6 @@ const router: Router = Router();
 
 import * as controller from "../../controllers/client/tour.controller";
 
-router.get("/", controller.index);
+router.get("/:slugCategory", controller.getToursBaseOnCategrorySlug);
 
 export const tourRoutes: Router = router;
