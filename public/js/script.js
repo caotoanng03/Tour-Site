ca196721321a26a5528f5cde2746f1b57bfeb23e
CaoToan <caotoan940@gmail.com>
2023-12-23 18:45:48 +0700

    Add a cart to localStorage

--- public/js/script.js
+++ public/js/script.js
@@ -16,3 +16,40 @@ var imagesMain = new Swiper(".imagesMain", {
     },
 });
 // End Slide Tour Detail
+
+// Cart
+const cart = localStorage.getItem("cart");
+
+// if not existed cart in localStorage, then create new one
+if (!cart) {
+    localStorage.setItem("cart", JSON.stringify([]))
+};
+
+// if existed, then add items to cart
+const formAddToCart = document.querySelector("[form-add-to-cart]");
+if (formAddToCart) {
+    formAddToCart.addEventListener("submit", (event) => {
+        event.preventDefault();
+
+        const quantity = parseInt(event.target.elements.quantity.value);
+        const tourId = parseInt(formAddToCart.getAttribute("tour-id"));
+
+        if (quantity > 0 && tourId) {
+            const cart = JSON.parse(localStorage.getItem("cart"));
+
+            const indexExistTour = cart.findIndex(item => item.tourId == tourId);
+
+            if (indexExistTour == -1) {
+                cart.push({
+                    tourId: tourId,
+                    quantity: quantity
+                });
+            } else {
+                cart[indexExistTour].quantity += quantity;
+            };
+            localStorage.setItem("cart", JSON.stringify(cart));
+        };
+    });
+};
+
+// End Cart
