b6a8f3fb3ce57240537034067628ec3c7647f17f
CaoToan <caotoan940@gmail.com>
2023-12-25 06:34:37 +0700

    Delete tour item in cart feature

--- public/js/cart.js
+++ public/js/cart.js
@@ -1,33 +1,59 @@
+
 // fetch data from servers and render tour items table
-fetch("/cart/list-json", {
-    method: "POST",
-    headers: {
-        "Content-Type": "application/json"
-    },
-    body: localStorage.getItem("cart")
-})
-    .then(res => res.json())
-    .then(data => {
-        const htmls = data.tours.map((element, index) => {
-            return `
-            <tr>
-                <td>${index + 1}</td>
-                <td><img src=${element.image} alt=${element.info.title} width="80px" /></td>
-                <td><a href="/tours/detail/${element.info.slug}">${element.info.title}</a></td>
-                <td>${element.discounted_price}VND</td>
-                <td><input type="number" name="quantity" value=${element.quantity} min="1" item-id="${element.info.id}" style="width: 60px" /></td>
-                <td>${element.total}VND</td>
-                <td><button class="btn btn-danger" btn-delete="${element.info.id}">Remove</button></td>
-            </tr>
-            `;
+const renderTourListInCart = () => {
+    fetch("/cart/list-json", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: localStorage.getItem("cart")
+    })
+        .then(res => res.json())
+        .then(data => {
+            const htmls = data.tours.map((element, index) => {
+                return `
+                <tr>
+                    <td>${index + 1}</td>
+                    <td><img src=${element.image} alt=${element.info.title} width="80px" /></td>
+                    <td><a href="/tours/detail/${element.info.slug}">${element.info.title}</a></td>
+                    <td>${element.discounted_price}VND</td>
+                    <td><input type="number" name="quantity" value=${element.quantity} min="1" item-id="${element.info.id}" style="width: 60px" /></td>
+                    <td>${element.total}VND</td>
+                    <td><button class="btn btn-danger" btn-delete="${element.info.id}">Remove</button></td>
+                </tr>
+                `;
+            });
+
+            const wrapperElem = document.querySelector("[tour-list]");
+            wrapperElem.innerHTML = htmls.join("");
+
+            // summary order price
+            const totalOrder = data.tours.reduce((acc, elem) => acc + elem.total, 0);
+            const totalOrderElem = document.querySelector("[total-order]");
+            totalOrderElem.innerText = totalOrder.toLocaleString();
+            deleteTourInCart();
         });
-
-        const wrapperElem = document.querySelector("[tour-list]");
-        wrapperElem.innerHTML = htmls.join("");
-
-        // summary order price
-        const totalOrder = data.tours.reduce((acc, elem) => acc + elem.total, 0);
-        const totalOrderElem = document.querySelector("[total-order]");
-        totalOrderElem.innerText = totalOrder.toLocaleString();
-    });
+}
+renderTourListInCart();
 // End fetch data from servers and render tour items table
+
+// Remove tour item in cart
+const deleteTourInCart = () => {
+    const listButtonDelete = document.querySelectorAll("[btn-delete]")
+    listButtonDelete.forEach(button => {
+        button.addEventListener("click", () => {
+            const tourId = button.getAttribute("btn-delete");
+            const cart = JSON.parse(localStorage.getItem("cart"));
+            const notDeletedItems = cart.filter(elem => elem.tourId != tourId);
+            localStorage.setItem("cart", JSON.stringify(notDeletedItems));
+
+            renderTourListInCart();
+        });
+    });
+};
+// End Remove tour item in cart
+
+
+
+
+
