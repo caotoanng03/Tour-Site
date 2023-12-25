c8fee9fa99cac8e9876e50d3395734232c345371
CaoToan <caotoan940@gmail.com>
2023-12-25 07:05:23 +0700

    Update quantity in cart feature

--- public/js/cart.js
+++ public/js/cart.js
@@ -1,5 +1,5 @@
 
-// fetch data from servers and render tour items table
+// Fetch data from servers and render tour items table
 const renderTourListInCart = () => {
     fetch("/cart/list-json", {
         method: "POST",
@@ -16,9 +16,9 @@ const renderTourListInCart = () => {
                     <td>${index + 1}</td>
                     <td><img src=${element.image} alt=${element.info.title} width="80px" /></td>
                     <td><a href="/tours/detail/${element.info.slug}">${element.info.title}</a></td>
-                    <td>${element.discounted_price}VND</td>
+                    <td>${element.discounted_price.toLocaleString()}VND</td>
                     <td><input type="number" name="quantity" value=${element.quantity} min="1" item-id="${element.info.id}" style="width: 60px" /></td>
-                    <td>${element.total}VND</td>
+                    <td>${element.total.toLocaleString()}VND</td>
                     <td><button class="btn btn-danger" btn-delete="${element.info.id}">Remove</button></td>
                 </tr>
                 `;
@@ -31,13 +31,15 @@ const renderTourListInCart = () => {
             const totalOrder = data.tours.reduce((acc, elem) => acc + elem.total, 0);
             const totalOrderElem = document.querySelector("[total-order]");
             totalOrderElem.innerText = totalOrder.toLocaleString();
+
             deleteTourInCart();
+            updateQuantityInCart();
         });
 }
 renderTourListInCart();
 // End fetch data from servers and render tour items table
 
-// Remove tour item in cart
+// Remove tour item
 const deleteTourInCart = () => {
     const listButtonDelete = document.querySelectorAll("[btn-delete]")
     listButtonDelete.forEach(button => {
@@ -51,9 +53,26 @@ const deleteTourInCart = () => {
         });
     });
 };
-// End Remove tour item in cart
+// End Remove tour item
 
+// Update quantity
+const updateQuantityInCart = () => {
+    const listInputQuantity = document.querySelectorAll("input[name=quantity]");
+    listInputQuantity.forEach(input => {
+        input.addEventListener("change", (event) => {
+            const updatedQuantity = parseInt(event.target.value);
+            const tourId = input.getAttribute("item-id");
 
+            const cart = JSON.parse(localStorage.getItem("cart"));
+            const tour = cart.find(elem => elem.tourId == tourId);
+            tour.quantity = updatedQuantity;
+            localStorage.setItem("cart", JSON.stringify(cart));
+
+            renderTourListInCart();
+        });
+    });
+};
+// End Update quantity
 
 
 
